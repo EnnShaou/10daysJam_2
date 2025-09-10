1bc7d030466d9e21a7f7a6f2e36a420c328fdd6b
Joshua Junaidi <k024g1040@g.neec.ac.jp>
2025-09-10 16:04:11 +0900

    オーディオ修正

--- Game.cpp
+++ Game.cpp
@@ -538,10 +538,12 @@ void Game::ChangePhase() {
 		//}
 		break;
 	case Game::Phase::kFadeOut:
+		bgm_->Stop();
 		fade_->Update(); // フェードの更新
 		if (player_->IsClear()) {
 			if (fade_->IsFinished()) {   // フェードアウトが完了したらシーンを終了
 				SceneNo = Scene::kMenu; // シーンを終了
+				
 			}
 		}
 		//else if (player_->IsDead()) {
@@ -551,7 +553,6 @@ void Game::ChangePhase() {
 		//}
 		else {
 			if (fade_->IsFinished()) {   // フェードアウトが完了したらシーンを終了
-				bgm_->Stop();
 				SceneNo = Scene::kReset; // シーンを終了
 			}
 		}
